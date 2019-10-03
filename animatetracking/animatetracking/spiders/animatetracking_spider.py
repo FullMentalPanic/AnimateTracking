3e47b649f5a95b976a5cce51ef6fde932f11aaf2
Liang <ganliang0122@gmail.com>
2019-10-03 15:59:02 +0800

    fix  scrapy and can get latest animate list

--- animatetracking/animatetracking/spiders/animatetracking_spider.py
+++ animatetracking/animatetracking/spiders/animatetracking_spider.py
@@ -5,6 +5,7 @@ from animatetracking.items import *
 import re
 import logging
 import datetime
+import time
 
 date_dict ={
     '一': '01',
@@ -20,29 +21,36 @@ class animatetracking(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for item in self.animateseason(response):
-            yield scrapy.Request(item['link'], callback=self.animatelist)
-
-    def animateseason(self,response):
-        now = datetime.date.now()
+        now = datetime.datetime.now()
         max_date = now + datetime.timedelta(weeks = 13)
         min_date = now - datetime.timedelta(weeks = 13)
+        link_list = []
         for resource in response.xpath("//div[@class='post-info pos-r pd10 post-side']/h2[@class='entry-title']"):           
             item = AnimatetrackingItem()
             item['link'] = resource.css('a::attr(href)').extract()[0]
             item['title'] = resource.css('a *::text').extract()[0]
             date = item['title']
-            item['date'] = date[0:4]+'-'+date_dict[date[5]]+'-01'+ +'-'
+            item['date'] = date[0:4]+'-'+date_dict[date[5]]+'-01'
             date_time = datetime.datetime.strptime(item['date'], '%Y-%m-%d')
-            if ((min_date <= data_time) and (data_time <= max_date)):
+            if ((min_date <= date_time) and (date_time <= max_date)):
+                link_list.append(item['link'])
                 yield item
-            else:
-                pass
-    
+        for link in link_list:
+            yield scrapy.Request(link, callback=self.animatelist)
+        #yield scrapy.Request(link_list[1], callback=self.animatelist)
+   
     def animatelist(self,response):
-        for resource in response.xpath("//h2/strong"):
+        count = 0
+        for resource in response.xpath("//div[@id='content-innerText']/h2"):
             item = AnimatelistItem()
-            item['animatetitle'] = resource.xpath('span/text()').extract_first()
-            item['introducation'] = resource.xpath("../following-sibling::ul[1]/li[2]/text()").extract_first()
+            sel = resource.xpath('strong/span/text()')
+            item['animatetitle'] = sel.extract_first()
+            if item['animatetitle'] is None:
+                sel = resource.xpath('span/strong/text()')
+                item['animatetitle'] = sel.extract_first()
+            item['introducation'] = resource.xpath("following-sibling::ul[1]/li[2]/text()").extract_first()
             yield item
+            count = count + 1
+        print ("This season total animate is ", count)
+
 
