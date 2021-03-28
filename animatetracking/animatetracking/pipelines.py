8f0316c42bff1c195561d52984590d0783b30c5a
FullMentalPanic <ganliang0122@gmail.com>
2021-03-28 23:34:44 +0800

    run spider by script

--- animatetracking/animatetracking/pipelines.py
+++ animatetracking/animatetracking/pipelines.py
@@ -12,38 +12,33 @@ from animatetracking import settings
 
 DEBUG = 0
 class AnimatetrackingPipeline(object):
-
     def __init__(self):
-        if DEBUG is 0:
-            self.connect = pymysql.connect(
-                host=settings.MYSQL_HOST,
-                db=settings.MYSQL_DBNAME,
-                user=settings.MYSQL_USER,
-                passwd=settings.MYSQL_PASSWD,
-                charset='utf8',
-                use_unicode=True)
-            self.cursor = self.connect.cursor()
+        self.connect = pymysql.connect(
+            host=settings.MYSQL_HOST,
+            db=settings.MYSQL_DBNAME,
+            user=settings.MYSQL_USER,
+            passwd=settings.MYSQL_PASSWD,
+            charset='utf8',
+            use_unicode=True)
+        self.cursor = self.connect.cursor()
 
     def process_item(self, item, spider):
-        if (DEBUG is 0):
-            #create new table 
-            sql = """SELECT COUNT(*) FROM information_schema.tables WHERE table_name = \'"""+item["table"]+ """\'"""         
+        #create new table 
+        sql = """SELECT COUNT(*) FROM information_schema.tables WHERE table_name = \'"""+item["table"]+ """\'"""         
+        self.cursor.execute(sql)
+        if self.cursor.fetchone()[0] == 0:
+            sql = "CREATE TABLE " +item["table"]+" (id INT AUTO_INCREMENT PRIMARY KEY, animatetitle VARCHAR(255), othertitle text, cross_s VARCHAR(255), nums INT, last_title VARCHAR(255))"
             self.cursor.execute(sql)
-            #print(self.cursor.fetchone()[0])
-            if self.cursor.fetchone()[0] == 0:
-                sql = "CREATE TABLE " +item["table"]+" (id INT AUTO_INCREMENT PRIMARY KEY, animatetitle VARCHAR(255), othertitle text, cross_s VARCHAR(255), nums INT, last_title VARCHAR(255))"
-                self.cursor.execute(sql)
-                sql = "ALTER TABLE " + item["table"]+ " CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci"
-                self.cursor.execute(sql)
+            sql = "ALTER TABLE " + item["table"]+ " CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci"
+            self.cursor.execute(sql)
 
-            sql = """select * from {} where animatetitle = \'{}\'""".format(item["table"],item["animatetitle"])
-            self.cursor.execute(sql)
-            if not self.cursor.fetchone():
-                sql = """INSERT INTO {} (animatetitle, othertitle, cross_s, nums, last_title) VALUE (\'{}\',\'{}\',\'{}\', \'{}\',\'{}\')""".format(item["table"],item["animatetitle"],item["othertitle"],item["cross"],item["nums"],item["last_title"])
-                self.cursor.execute(sql)                                
-                self.connect.commit()
+        sql = """select * from {} where animatetitle = \'{}\'""".format(item["table"],item["animatetitle"])
+        self.cursor.execute(sql)
+        if not self.cursor.fetchone():
+            sql = """INSERT INTO {} (animatetitle, othertitle, cross_s, nums, last_title) VALUE (\'{}\',\'{}\',\'{}\', \'{}\',\'{}\')""".format(item["table"],item["animatetitle"],item["othertitle"],item["cross"],item["nums"],item["last_title"])
+            self.cursor.execute(sql)                                
+            self.connect.commit()
         return item
 
     def close_spider(self, spider):
-        if DEBUG is 0:
-            self.connect.close()
+        self.connect.close()
--- animatetracking/main.py
+++ animatetracking/main.py
@@ -1,8 +1,12 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from animatetracking import settings as my_settings
 from animatetracking.spiders.animatetracking_spider import animatetracking
 
 if __name__ == "__main__":
-  process = CrawlerProcess()
+  crawler_settings = Settings()
+  crawler_settings.setmodule(my_settings)
+  process = CrawlerProcess(settings=crawler_settings)
   process.crawl(animatetracking)
   process.start()
